8a2fc368eae730994e0468ae44b3310b4d1ff007
terjemahin <81403024+terjemahin@users.noreply.github.com>
2024-12-02 15:43:25 +0700

    Add files via upload

---
+++ assets/title.js
@@ -0,0 +1,14 @@
+const dataDir = {
+    "The Eternal Supreme": {
+        "url": "https://terjemahin.github.io/website/the-eternal-supreme",
+        "date": "2024-01-01",
+        "total": "2406",
+        "status": "Completed"
+    },
+    "The Ultimate of All Ages": {
+        "url": "https://terjemahin.github.io/website/the-ultimate-of-all-ages",
+        "date": "2024-01-01",
+        "total": " 1478",
+        "status": "Ongoing"
+    }
+};
