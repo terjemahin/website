0bcb37d1bf3fff2ab7ebb0ccecf940657893b057
terjemahin <81403024+terjemahin@users.noreply.github.com>
2024-12-02 10:28:16 +0700

    Create 0101-0200.js

---
+++ the-eternal-supreme/data/0101-0200.js
@@ -0,0 +1,122 @@
+data = {
+    ...data,
+"Chapter 0101": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0101",
+    "date": "2024-01-01",
+    "size": " 36.47 KB"
+},
+
+"Chapter 0102": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0102",
+    "date": "2024-01-02",
+    "size": " 28.52 KB"
+},
+
+"Chapter 0103": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0103",
+    "date": "2024-01-03",
+    "size": " 32.89 KB"
+},
+
+"Chapter 0104": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0104",
+    "date": "2024-01-04",
+    "size": " 44.21 KB"
+},
+
+"Chapter 0105": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0105",
+    "date": "2024-01-05",
+    "size": " 26.73 KB"
+},
+
+"Chapter 0106": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0106",
+    "date": "2024-01-06",
+    "size": " 41.37 KB"
+},
+
+"Chapter 0107": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0107",
+    "date": "2024-01-07",
+    "size": " 38.55 KB"
+},
+
+"Chapter 0108": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0108",
+    "date": "2024-01-08",
+    "size": " 27.99 KB"
+},
+
+"Chapter 0109": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0109",
+    "date": "2024-01-09",
+    "size": " 30.24 KB"
+},
+
+"Chapter 0110": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0110",
+    "date": "2024-01-10",
+    "size": " 43.12 KB"
+},
+
+"Chapter 0111": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0111",
+    "date": "2024-01-11",
+    "size": " 29.83 KB"
+},
+
+"Chapter 0112": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0112",
+    "date": "2024-01-12",
+    "size": " 35.65 KB"
+},
+
+"Chapter 0113": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0113",
+    "date": "2024-01-13",
+    "size": " 42.07 KB"
+},
+
+"Chapter 0114": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0114",
+    "date": "2024-01-14",
+    "size": " 25.48 KB"
+},
+
+"Chapter 0115": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0115",
+    "date": "2024-01-15",
+    "size": " 40.13 KB"
+},
+
+"Chapter 0116": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0116",
+    "date": "2024-01-16",
+    "size": " 33.72 KB"
+},
+
+"Chapter 0117": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0117",
+    "date": "2024-01-17",
+    "size": " 28.95 KB"
+},
+
+"Chapter 0118": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0118",
+    "date": "2024-01-18",
+    "size": " 39.47 KB"
+},
+
+"Chapter 0119": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0119",
+    "date": "2024-01-19",
+    "size": " 31.82 KB"
+},
+
+"Chapter 0120": {
+    "url": "https://terjemahin.github.io/website/the-eternal-supreme/chapter-0120",
+    "date": "2024-01-20",
+    "size": " 37.21 KB"
+}
+}
