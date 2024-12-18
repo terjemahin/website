5ca938cc8d327d5a5ae43755c0bcd2a9a7c75bf8
terjemahin <akunmail0012@gmail.com>
2024-12-18 22:50:41 +0700

    add return-of-the-mount-hua-sect

---
+++ return-of-the-mount-hua-sect/data/list-dir.js
@@ -0,0 +1,14 @@
+dataDir = {
+    "Chapter 0000-0098": {"url": "/return-of-the-mount-hua-sect/part?=1", "date": "2024-12-12", "size": "3.8 MB"},
+    "Chapter 0099-0198": {"url": "/return-of-the-mount-hua-sect/part?=2", "date": "2024-12-12", "size": "4.2 MB"},
+    "Chapter 0199-0298": {"url": "/return-of-the-mount-hua-sect/part?=3", "date": "2024-12-12", "size": "4.5 MB"},
+    "Chapter 0299-0398": {"url": "/return-of-the-mount-hua-sect/part?=4", "date": "2024-12-12", "size": "4.3 MB"},
+    "Chapter 0399-0498": {"url": "/return-of-the-mount-hua-sect/part?=5", "date": "2024-12-12", "size": "4.5 MB"},
+    "Chapter 0499-0598": {"url": "/return-of-the-mount-hua-sect/part?=6", "date": "2024-12-12", "size": "4.2 MB"},
+    "Chapter 0599-0698": {"url": "/return-of-the-mount-hua-sect/part?=7", "date": "2024-12-12", "size": "3.9 MB"},
+    "Chapter 0699-0798": {"url": "/return-of-the-mount-hua-sect/part?=8", "date": "2024-12-12", "size": "4.1 MB"},
+    "Chapter 0799-0898": {"url": "/return-of-the-mount-hua-sect/part?=9", "date": "2024-12-12", "size": "4.1 MB"},
+    "Chapter 0899-0998": {"url": "/return-of-the-mount-hua-sect/part?=10", "date": "2024-12-12", "size": "4.1 MB"},
+    "Chapter 0999-1098": {"url": "/return-of-the-mount-hua-sect/part?=11", "date": "2024-12-12", "size": "3.9 MB"},
+    "Chapter 1099-1144": {"url": "/return-of-the-mount-hua-sect/part?=12", "date": "2024-12-12", "size": "1.9 MB"},
+};
