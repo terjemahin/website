3b3778a4a0dbf598dcd52039c2b3573d7c7ed0c4
terjemahin <akunmail0012@gmail.com>
2024-12-18 22:50:29 +0700

    add reformation-deadbeat-noble

---
+++ reformation-deadbeat-noble/data/list-dir.js
@@ -0,0 +1,22 @@
+const dataDir = {
+  "Chapter Prologue-098": {
+    url: "/reformation-deadbeat-noble/part?=1",
+    date: "2024-12-14",
+    size: " 3.3 MB",
+  },
+  "Chapter 099-198": {
+    url: "/reformation-deadbeat-noble/part?=2",
+    date: "2024-12-14",
+    size: " 3.5 MB",
+  },
+  "Chapter 199-298": {
+    url: "/reformation-deadbeat-noble/part?=3",
+    date: "2024-12-14",
+    size: " 3.4 MB",
+  },
+  "Chapter 299-END": {
+    url: "/reformation-deadbeat-noble/part?=4",
+    date: "2024-12-14",
+    size: " 2.9 MB",
+  },
+};
