6acf06bd71423966656e36071798e42b1babd02e
terjemahin <akunmail0012@gmail.com>
2024-12-18 22:51:12 +0700

    add the-worlds-best-engineer

---
+++ the-worlds-best-engineer/data/list-dir.js
@@ -0,0 +1,7 @@
+dataDir = {
+    "Chapter 001-100": {"url": "/the-worlds-best-engineer/part?=1", "date": "2024-12-12", "size": "5.2 MB"},
+    "Chapter 101-200": {"url": "/the-worlds-best-engineer/part?=2", "date": "2024-12-12", "size": "5.1 MB"},
+    "Chapter 201-300": {"url": "/the-worlds-best-engineer/part?=3", "date": "2024-12-12", "size": "5.2 MB"},
+    "Chapter 301-400": {"url": "/the-worlds-best-engineer/part?=4", "date": "2024-12-12", "size": "5.0 MB"},
+    "Chapter 401-END": {"url": "/the-worlds-best-engineer/part?=5", "date": "2024-12-12", "size": "438.3 KB"},
+};
---
+++ the-worlds-best-engineer/data/part-5.js
@@ -0,0 +1,11 @@
+data = {
+    ...data,
+    "Chapter 401": {"url": "chapter-401", "date": "2024-12-12", "size": "51.0 KB"},
+    "Chapter 402": {"url": "chapter-402", "date": "2024-12-12", "size": "62.2 KB"},
+    "Chapter 403": {"url": "chapter-403", "date": "2024-12-12", "size": "50.5 KB"},
+    "Chapter 404": {"url": "chapter-404", "date": "2024-12-12", "size": "51.1 KB"},
+    "Chapter 405": {"url": "chapter-405", "date": "2024-12-12", "size": "58.5 KB"},
+    "Chapter 406": {"url": "chapter-406", "date": "2024-12-12", "size": "54.8 KB"},
+    "Chapter 407": {"url": "chapter-407", "date": "2024-12-12", "size": "61.3 KB"},
+    "Chapter 408": {"url": "chapter-408", "date": "2024-12-12", "size": "48.9 KB"},
+};
