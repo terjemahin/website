d2e2a75aa66cd88838a23187ab7274b54c1bb68e
terjemahin <akunmail0012@gmail.com>
2024-12-28 16:36:03 +0700

    add the-eternal-supreme

---
+++ the-eternal-supreme/data/part-42.js
@@ -0,0 +1,9 @@
+data = {
+    ...data,
+    "Chapter 2401": {"url": "/the-eternal-supreme/2/chapter-2401", "date": "2024-12-28", "size": "34.8 KB"},
+    "Chapter 2402": {"url": "/the-eternal-supreme/2/chapter-2402", "date": "2024-12-28", "size": "35.4 KB"},
+    "Chapter 2403": {"url": "/the-eternal-supreme/2/chapter-2403", "date": "2024-12-28", "size": "34.7 KB"},
+    "Chapter 2404": {"url": "/the-eternal-supreme/2/chapter-2404", "date": "2024-12-28", "size": "35.8 KB"},
+    "Chapter 2405": {"url": "/the-eternal-supreme/2/chapter-2405", "date": "2024-12-28", "size": "36.9 KB"},
+    "Chapter 2406": {"url": "/the-eternal-supreme/2/chapter-2406", "date": "2024-12-28", "size": "52.1 KB"},
+};
